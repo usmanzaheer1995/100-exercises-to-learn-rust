ad20cefd11ef4cf15c06db3756610d662bb61266
Usman Zaheer <usmanzaheer1995@gmail.com>
2025-12-15 20:47:48 +0100

    section 3 completed

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -5,6 +5,67 @@
 //   The unit price is in cents and must be strictly greater than zero.
 //   Order must include a method named `total` that returns the total price of the order.
 //   Order must provide setters and getters for each field.
+pub struct Order {
+    product_name: String,
+    quantity: u32,
+    unit_price: u32
+}
+
+impl Order {
+    pub fn new(product_name: String, quantity: u32, unit_price: u32) -> Order {
+        validate_product_name(&product_name);
+        validate_quantity(quantity);
+        validate_unit_price(unit_price);
+        Order { product_name, quantity, unit_price }
+    }
+
+    pub fn total(&self) -> u32 {
+        self.quantity * self.unit_price
+    }
+
+    pub fn set_product_name(&mut self, product_name: String) {
+        self.product_name = product_name;
+    }
+
+    pub fn product_name(&self) -> &String {
+        &self.product_name
+    }
+
+    pub fn set_quantity(&mut self, quantity: u32) {
+        self.quantity = quantity;
+    }
+
+    pub fn quantity(&self) -> &u32 {
+        &self.quantity
+    }
+
+    pub fn set_unit_price(&mut self, unit_price: u32) {
+        self.unit_price = unit_price;
+    }
+
+    pub fn unit_price(&self) -> &u32 {
+        &self.unit_price
+    }
+}
+
+fn validate_product_name(product_name: &str) {
+    if product_name.is_empty() || product_name.len() > 300 {
+        panic!("Invalid product name");
+    }
+}
+
+fn validate_quantity(quantity: u32) {
+    if quantity == 0 {
+        panic!("Quantity must be strictly greater than zero");
+    }
+}
+
+fn validate_unit_price(unit_price: u32) {
+    if unit_price == 0 {
+        panic!("Unit price must be strictly greater than zero");
+    }
+}
+
 //
 // Tests are located in a different place this time—in the `tests` folder.
 // The `tests` folder is a special location for `cargo`. It's where it looks for **integration tests**.
