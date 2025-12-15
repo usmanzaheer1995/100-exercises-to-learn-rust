0dc203c7ceb9dbc5292dd19b1c9c5ea400b1ba7f
Usman Zaheer <usmanzaheer1995@gmail.com>
2025-12-15 23:48:47 +0100

    04.02: derive macros

--- exercises/04_traits/01_trait/src/lib.rs
+++ exercises/04_traits/01_trait/src/lib.rs
@@ -1,7 +1,23 @@
 // Define a trait named `IsEven` that has a method `is_even` that returns a `true` if `self` is
 // even, otherwise `false`.
+
+trait IsEven {
+    fn is_even(self) -> bool;
+}
+
 //
 // Then implement the trait for `u32` and `i32`.
+impl IsEven for u32 {
+    fn is_even(self) -> bool {
+        self % 2 == 0
+    }
+}
+
+impl IsEven for i32 {
+    fn is_even(self) -> bool {
+        self % 2 == 0
+    }
+}
 
 #[cfg(test)]
 mod tests {
