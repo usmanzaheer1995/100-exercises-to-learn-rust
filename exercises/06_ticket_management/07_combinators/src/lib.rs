11ad99c4f5b246af288e9e0035e16e6f7774da37
Usman Zaheer <usmanzaheer1995@gmail.com>
2026-01-01 17:16:10 +0100

    complete combinators exercise

--- exercises/06_ticket_management/07_combinators/src/lib.rs
+++ exercises/06_ticket_management/07_combinators/src/lib.rs
@@ -31,6 +31,13 @@ impl TicketStore {
     pub fn add_ticket(&mut self, ticket: Ticket) {
         self.tickets.push(ticket);
     }
+
+    pub fn to_dos(&self) -> Vec<&Ticket> {
+        self.tickets
+            .iter()
+            .filter(|item| item.status == Status::ToDo)
+            .collect::<Vec<&Ticket>>()
+    }
 }
 
 #[cfg(test)]
