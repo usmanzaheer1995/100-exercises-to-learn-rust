fdf630f7d2e8d8306636b30c8b2467744444dcbb
Usman Zaheer <usmanzaheer1995@gmail.com>
2026-01-02 23:05:05 +0100

    complete section 6

--- exercises/06_ticket_management/12_two_states/src/lib.rs
+++ exercises/06_ticket_management/12_two_states/src/lib.rs
@@ -11,6 +11,7 @@ use ticket_fields::{TicketDescription, TicketTitle};
 #[derive(Clone)]
 pub struct TicketStore {
     tickets: Vec<Ticket>,
+    counter: u64,
 }
 
 #[derive(Clone, Copy, Debug, PartialEq)]
@@ -41,11 +42,24 @@ impl TicketStore {
     pub fn new() -> Self {
         Self {
             tickets: Vec::new(),
+            counter: 0,
         }
     }
 
-    pub fn add_ticket(&mut self, ticket: Ticket) {
-        self.tickets.push(ticket);
+    pub fn get(&self, id: TicketId) -> Option<&Ticket> {
+        self.tickets.iter().find(|t| t.id == id)
+    }
+
+    pub fn add_ticket(&mut self, ticket: TicketDraft) -> TicketId {
+        let id = self.counter;
+        self.counter += 1;
+        self.tickets.push(Ticket {
+            id: TicketId(id),
+            title: ticket.title,
+            description: ticket.description,
+            status: Status::ToDo,
+        });
+        TicketId(id)
     }
 }
 
